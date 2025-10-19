70ce6c4ad35de8548714106ba303a3555da574a9
Appropriate32 <eissadar3218@gmail.com>
2025-10-19 22:40:42 +0500

    Made the win condition method

--- script.js
+++ script.js
@@ -16,10 +16,9 @@ const gameBoard = (function() {
             }
         },
         reset: function() {
-            gameBoard = [null, null, null, 
-                        null, null, null, 
-                        null, null, null
-                        ]
+            for (let i = 0; i < gameBoard.length; i++) {
+                gameBoard[i] = null;
+            }
         }
     }
 })();
@@ -28,10 +27,24 @@ const gameController = (function () {
     let players = [];
     let currentIndex = 0;
     let gameActive = false;
+    let winConditions = [[0, 1, 2], [3, 4 , 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+    let win = false;
 
     const switchPlayer = () => {
         currentIndex = currentIndex === 0 ? 1 : 0;
     };
+
+
+    const checkWin = () => {
+        win = false;
+        const boardTemp = gameBoard.getBoard();
+        const matchesCondition = (currentValue) => boardTemp[currentValue] === players[currentIndex].marker;
+
+        for (const condition of winConditions) {
+            win = condition.every(matchesCondition);
+            if (win) { break;};
+        }
+};
 
     return {
         startGame : function(player1Name, player2Name) {
@@ -42,6 +55,7 @@ const gameController = (function () {
             currentIndex = 0;
             gameBoard.reset();
             gameActive = true;
+            win = false;
 
             console.log(`${players[currentIndex].name}'s turn!`);
             
@@ -54,7 +68,15 @@ const gameController = (function () {
             const currentPlayer = players[currentIndex];
             const success = gameBoard.setMark(index, currentPlayer.marker);
             if (!success) return console.log('Spot taken!');
-
+            
+            checkWin();
+            if (win) {
+                console.log(`${players[currentIndex].name} wins!`);
+                currentPlayer.incrementScore();
+                gameActive = false;
+                console.log(`Scores: ${players[0].name}: ${players[0].getScore()}, ${players[1].name}: ${players[1].getScore()}`);
+                return;
+            }
             switchPlayer();
             console.log(`${players[currentIndex].name}'s turn!`);
         }
@@ -74,5 +96,7 @@ function createPlayer(name, marker) {
 
 gameController.startGame("Eissa", "Moosa");
 gameController.playRound(3);
+gameController.playRound(2);
+gameController.playRound(4);
+gameController.playRound(1);
 gameController.playRound(5);
-gameController.playRound(0);
