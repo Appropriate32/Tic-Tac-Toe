84998531bfeb8b2b36a9609c798a5f330aa8d354
Appropriate32 <eissadar3218@gmail.com>
2025-10-21 20:32:31 +0500

    Added a check for ties and returned objects too

--- script.js
+++ script.js
@@ -29,21 +29,23 @@ const gameController = (function () {
     let gameActive = false;
     let winConditions = [[0, 1, 2], [3, 4 , 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
     let win = false;
+    let moveCount = 0;
 
     const switchPlayer = () => {
         currentIndex = currentIndex === 0 ? 1 : 0;
     };
 
 
-    const checkWin = () => {
+    const isWin = () => {
         win = false;
         const boardTemp = gameBoard.getBoard();
         const matchesCondition = (currentValue) => boardTemp[currentValue] === players[currentIndex].marker;
 
         for (const condition of winConditions) {
             win = condition.every(matchesCondition);
-            if (win) { break;};
+            if (win) { break};
         }
+        return win;
 };
 
     return {
@@ -56,6 +58,7 @@ const gameController = (function () {
             gameBoard.reset();
             gameActive = true;
             win = false;
+            moveCount = 0;
 
             console.log(`${players[currentIndex].name}'s turn!`);
             
@@ -64,17 +67,30 @@ const gameController = (function () {
             return players[currentIndex];
         },
         playRound(index) {
+            if ((index < 0 || index > 8) || (typeof index !== 'number')) return;
             if (!gameActive) return;
             const currentPlayer = players[currentIndex];
             const success = gameBoard.setMark(index, currentPlayer.marker);
             if (!success) return console.log('Spot taken!');
-            
-            checkWin();
-            if (win) {
-                console.log(`${players[currentIndex].name} wins!`);
+
+            moveCount++;
+            if (isWin()) {
                 currentPlayer.incrementScore();
                 gameActive = false;
-                console.log(`Scores: ${players[0].name}: ${players[0].getScore()}, ${players[1].name}: ${players[1].getScore()}`);
+                
+                return {ok: true, 
+                    result: "win", 
+                    winner: currentPlayer.name, 
+                    scores: {
+                        [players[0].name]: players[0].getScore(), 
+                        [players[1].name]: players[1].getScore()
+
+                    },
+                    message: `${currentPlayer.name} wins!\nScores - ${players[0].name}: ${players[0].getScore()}, ${players[1].name}: ${players[1].getScore()}`
+                };
+            } else if (moveCount === 9) {
+                console.log("It is a tie!");
+                gameActive = false;
                 return;
             }
             switchPlayer();
